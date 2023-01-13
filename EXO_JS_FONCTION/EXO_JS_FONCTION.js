db5db5604dc5cebc3a7f7041bd6bb3ff49983c7a
registredsc <shirozunregistre@gmail.com>
2023-01-13 11:16:07 +0100

    .

--- EXO_JS_FONCTION/EXO_JS_FONCTION.js
+++ EXO_JS_FONCTION/EXO_JS_FONCTION.js
@@ -15,3 +15,10 @@ function cube(x) {
     let text = document.getElementById("cube").innerHTML = "<p>Le cube de " + x + " est égal à " + x * x * x + "</p>";
     
 }
+
+
+//exo 2 Concevez la fonction strtok() qui prend 3 paramètres str1, str2, n en entrée et renvoie une chaîne de caractères : str1 est composée d'une liste de mots séparés par le caractère str2. strtok() sert à extraire le nième mot de str1.
+
+function strtok(str1, str2, n) {
+    let text = document.getElementById("strtok").innerHTML = "<p>Le nième mot de " + str1 + " est égal à " + str1.split(str2)[n] + "</p>";
+}
